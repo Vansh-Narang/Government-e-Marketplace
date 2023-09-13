4a55ec50d3825e363de57dcf87bf73ff5f856a5c
Vansh=Narang <vanshnarang49906@gmail.com>
2023-09-13 22:19:15 +0530

    Added Data and Checked if working

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
-import Cards from './Cards'
+import Cards from './Cards';
+import Data from "../Dummy.json";
+import { Link } from "react-router-dom"
 
 function Card() {
     const url = "https://fakestoreapi.com/products";
@@ -25,9 +27,9 @@ function Card() {
     return (
         <div>
             {
-                products.map((product) => (
+                Data.products.map((product, i) => (
                     <div>
-                        <div key={product.id}>
+                        <div key={i}>
                             <div>
                                 <p>{product.title}</p>
                             </div>
@@ -35,10 +37,18 @@ function Card() {
                                 <p>{product.description}</p>
                             </div>
                             <div>
-                                <img src={product.image} />
+                                <img src={product.thumbnail} />
                             </div>
                             <div>
                                 <p>{product.price}</p>
+                            </div>
+                            <div>
+                                {/* <button>Buy at Amazon</button>
+                                <p>{product.url_amazon}</p> */}
+                                {
+                                    <Link to={product.url_amazon} target='_blank'>Buy at Amazon</Link>
+                                }
+
                             </div>
                         </div>
 
