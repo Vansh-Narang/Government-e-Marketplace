7b654ec6eeb309177db3fb2e952a91161ef882e2
Vansh=Narang <vanshnarang49906@gmail.com>
2023-09-13 20:33:12 +0530

    Added Map functions and fetched Data

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,10 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Cards from './Cards'
+
 function Card() {
+    const url = "https://fakestoreapi.com/products";
+
+    const [products, setProducts] = useState([])
+
+    async function getProducts() {
+        try {
+            const response = await fetch(url)
+            // console.log(data)
+            const parsedData = await response.json()
+            console.log(parsedData)
+            setProducts(parsedData)
+            console.log("Data is ")
+            console.log(products)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    useEffect(() => {
+        getProducts()
+    },)
     return (
         <div>
-            {/* map function here to display cards */}
-            <Cards />
+            {
+                products.map((product) => (
+                    <div>
+                        <div key={product.id}>
+                            <div>
+                                <p>{product.title}</p>
+                            </div>
+                            <div>
+                                <p>{product.description}</p>
+                            </div>
+                            <div>
+                                <img src={product.image} />
+                            </div>
+                            <div>
+                                <p>{product.price}</p>
+                            </div>
+                        </div>
+
+                    </div>
+
+
+                ))
+            }
+
         </div>
     )
 }
