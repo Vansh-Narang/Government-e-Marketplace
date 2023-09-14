7d65b2f54cec7c2ca20189069d8812255f4c4701
Vansh=Narang <vanshnarang49906@gmail.com>
2023-09-14 22:56:40 +0530

    Sample implementation done

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import Card from './Components/Card';
 import SearchBar from './Components/SearchBar';
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import Spinner from './Spinner';
-import {Route,Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from './Components/Home';
 import About from "./Components/About";
 import Contactus from './Components/Contactus';
@@ -12,39 +12,40 @@ import Cards from './Components/Cards';
 function App() {
 
 
- const url = "https://fakestoreapi.com/products";
+  const url = "https://fakestoreapi.com/products";
 
-    const [products, setProducts] = useState([]);
-    const [loader,setloader]= useState(false);
+  const [products, setProducts] = useState([]);
+  const [loader, setloader] = useState(false);
 
-    async function getProducts() {
-        try {
-          setloader(true);
-            const response = await fetch(url)
-          
-            const parsedData = await response.json()
-            setloader(false);
-            setProducts(parsedData);
-          
-        } catch (error) {
-            console.log(error)
-        }
+  async function getProducts() {
+    try {
+      setloader(true);
+      const response = await fetch(url)
+
+      const parsedData = await response.json()
+      setProducts(parsedData);
+
+    } catch (error) {
+      console.log(error)
     }
-    useEffect(() => {
-        getProducts();
-    },[])
+    setloader(false);
+  }
+  useEffect(() => {
+    getProducts();
+  }, [])
   return (
     <div className=" h-screen w-screen">
-          <Navbar />
-          <SearchBar></SearchBar>
-          <div><Routes>
-          <Route path="/" Component={loader?<Spinner/>:<Cards/>} ></Route>
-          <Route path="/home" Component={<Home></Home>}></Route>
-            <Route path="/about" Component={<About></About>}></Route>
-            <Route path="/contactus" Component={<Contactus></Contactus>}></Route>
-            <Route path="*" Component={<h1>Page Not Found 404</h1>}></Route>
-          </Routes></div>
-          
+      <Navbar />
+      <SearchBar></SearchBar>
+      <div>
+        <Routes>
+          <Route path="/" element={loader ? <Spinner /> : <Cards />} ></Route>
+          <Route path="/home" element={<Home></Home>}></Route>
+          <Route path="/about" element={<About></About>}></Route>
+          <Route path="/contactus" element={<Contactus></Contactus>}></Route>
+          <Route path="*" element={<h1>Page Not Found 404</h1>}></Route>
+        </Routes></div>
+
     </div>
   );
 }
