acb9af7e15eaf91b775631e3770ecefb39dde6c8
soni895 <darshansoni895@gmail.com>
2023-09-23 13:30:09 +0530

    changes apply

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -11,12 +11,12 @@ function Card({product,title}) {
         <div className=' place relative bg-slate-700 text-white '>
         <img src={product.thumbnail} className=" w-[360px] aspect-[1/1]  object-cover  "></img>
          <h1 className="  text-4xl mt-4  flex items-center justify-center gap-2  text-green-400 tracking-widest">{product.price} <spam className="mt-2" ><BsCurrencyRupee/> </spam></h1>
-          <h4 className="text-gray-300">{product.category}</h4>
+         
         <h2 className=' text-gray-100 font-semibold  text-lg mt-5 leading-7' >
         <span className=' text-white font-bold'>Title:</span> {product.title}</h2>
         <p className=' text-gray-300'><span className=' font-bold text-white'>description: </span>{product.description}</p>
-        <p>{product.discountPercentage}</p>
-        <p>{product.stock}</p>
+        <p><span className=' text-white font-bold'>Discount :</span> {product.discountPercentage}</p>
+        <p><span className=' text-white font-bold'>Stock : </span>{product.stock}</p>
         {/* <p>{product.thumbnail}</p> */}
     
         {/* <img scr={product.thumbnail}></img> */}
